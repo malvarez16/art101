923aace94a2d62f769c5df7ae5e95da5becfe69e
malvarez16 <65634560+malvarez16@users.noreply.github.com>
2021-01-27 23:19:18 -0800

    lab 5
    this is lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/*
+  * Author: May Alvarez <mamialva@ucsc.edu>
+  * Created 27 January 2021
+  * License: Public Domain
+  */
+
+// Define Variables
+var make = "Ford";
+var model = "Escape";
+var color = "black";
+var year = 2013;
+
+//calculate
+var age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model " + model + "<br>");
+document.writeln("Color " + color + "<br>");
+document.writeln("Year " + year + "<br>");
